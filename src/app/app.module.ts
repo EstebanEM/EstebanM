729f6ff0ee680a1b0a7a1856a3fd6280feeb8e53
EstebanEM <139818678+EstebanEM@users.noreply.github.com>
2025-07-15 10:31:40 -0500

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IndexComponent } from './index/index.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { TextComponent } from './components/text/text.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     IndexComponent,
     HeaderComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    TextComponent
     
   ],
   imports: [
--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-index',
   standalone: false,
   templateUrl: './index.component.html',
   styleUrl: './index.component.sass'
 })
-export class IndexComponent  {
+export class IndexComponent {
 
 
 }
