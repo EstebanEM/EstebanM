e21fb44889ed0d28cb3cdd71021fe40ce35ab44b
EstebanEM <139818678+EstebanEM@users.noreply.github.com>
2025-07-15 10:40:45 -0500

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 7252, hash: '92bdbd33f9a3ea6e8046d66b09413a32f232b8342697b0ce069eb2f4478b5bfe', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 7307, hash: 'a3344a08f0c9e8924ee41c5e82811a89663478ffd032de235c7bcc85e236f5eb', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 26526, hash: 'f9a7185fde5e33eca1a84b547ae07e6fbce57752ec1315ca428642691934f4dc', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 7665, hash: 'c85cbfabb3d119f400961a490baca3c19e4b786fcb153773b4b234a82366ab08', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 7720, hash: '39d4e4d42163d390ac7e0c322b644eca007da57566ab3b1105b353e0f6fddbf3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 35842, hash: '097d38b15f7c1ccf1481d8a6e1045220aa0b37a7bae2be92acc3f77184e50de3', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-R6X6WWSU.css': {size: 540, hash: 'XE993wYb5Po', text: () => import('./assets-chunks/styles-R6X6WWSU_css.mjs').then(m => m.default)}
   },
 };
